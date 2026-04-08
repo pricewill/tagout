ae2adf89aea8fda2729c6f9157491105d6df5e06
Will Price <price.will@gmail.com>
2026-04-08 12:10:47 -0600

    hardcode auth redirect URL

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -20,7 +20,7 @@ export default function LoginPage() {
     const { error } = await supabase.auth.signInWithOtp({
       email,
       options: {
-        emailRedirectTo: `${process.env.NEXT_PUBLIC_APP_URL}/auth/callback`,
+        emailRedirectTo: `https://defectivegene.club/auth/callback`,
       },
     });
 
